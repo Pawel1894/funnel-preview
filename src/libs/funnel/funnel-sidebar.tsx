4e0d7a09b9cac7d868af53445514b71b70814b2d
Pawel Pohl <pawepohl@gmail.com>
2025-04-01 14:10:56 +0200

    Add scrolling behavior for funnel sidebar for large amount of pages

--- src/libs/funnel/funnel-sidebar.tsx
+++ src/libs/funnel/funnel-sidebar.tsx
@@ -12,16 +12,21 @@ export function FunnelSidebar({ pages, onPageSelection, currentPage }: FunnelSid
   const hasPages = pages && pages.length > 0;
 
   return (
-    <aside className="flex flex-col p-4 border border-muted rounded-md h-full">
-      <h2 className="text-lg font-semibold">Funnel pages</h2>
-      <nav className="mt-4 space-y-2">
+    <aside className="flex flex-col border border-muted rounded-md h-full min-h-0 overflow-hidden">
+      <nav className="mt-4 space-y-2 h-full">
         {hasPages ? (
-          <List className="w-full" selectedItem={currentPage?.id} onSelect={onPageSelection}>
-            {pages.map((page, i) => (
-              <ListItem title={createPageName(i)} className="truncate" key={page.id} id={page.id}>
-                {createPageName(i)}
-              </ListItem>
-            ))}
+          <List
+            className="w-full min-h-full h-0 overflow-y-auto"
+            selectedItem={currentPage?.id}
+            onSelect={onPageSelection}
+          >
+            <div className="px-4">
+              {pages.map((page, i) => (
+                <ListItem title={createPageName(i)} className="truncate" key={page.id} id={page.id}>
+                  {createPageName(i)}
+                </ListItem>
+              ))}
+            </div>
           </List>
         ) : (
           <span className="text-sm text-muted-foreground">No pages found</span>
